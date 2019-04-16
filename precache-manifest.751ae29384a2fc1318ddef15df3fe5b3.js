8e15465188f04e1ab960ffb21b3da9099909a373
chenyuhao <chenyuhao@chenyuhaodeMacBook-Pro.local>
2019-04-17 04:16:46 +0800

    Updates

---
+++ precache-manifest.751ae29384a2fc1318ddef15df3fe5b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9728fa41a930f12fdc87",
+    "url": "/tic-tac-toe/static/js/runtime~main.f1e29306.js"
+  },
+  {
+    "revision": "e0e37f59e1fff012ec3a",
+    "url": "/tic-tac-toe/static/js/main.0033e6da.chunk.js"
+  },
+  {
+    "revision": "9a5c0cc5b7b5942488d9",
+    "url": "/tic-tac-toe/static/js/2.56cb80cd.chunk.js"
+  },
+  {
+    "revision": "e0e37f59e1fff012ec3a",
+    "url": "/tic-tac-toe/static/css/main.10cb42df.chunk.css"
+  },
+  {
+    "revision": "4c5e670ad267f39a0dbfaefa4a845672",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
